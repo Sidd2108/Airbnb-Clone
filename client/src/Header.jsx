4f996516c0c8d9e421ffecaaef8bba8dcd030fba
Siddhesh Shinde <talktosiddhesh.shinde@gmail.com>
2023-06-28 09:36:52 +0530

    Day 5 - Final Commit

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,8 +5,11 @@ export default function Header() {
   const { user } = useContext(UserContext)
   return (
     <header className='  flex justify-between'>
-      <Link to={'/'} className="flex items-center " >
-        <img className='h-14' src="https://www.vectorlogo.zone/logos/airbnb/airbnb-ar21.svg" alt="airbnb logo" />
+      <Link to={'/'} className="flex gap-1 items-center " >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 -rotate-90 text-primary">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>
+        <h1 className="text-primary font-bold text-2xl pb-1">airbnb</h1>
       </Link>
       <div className='flex items-center gap-2 border border-gray rounded-full border-height py-2 px-4 shadow-md shadow-gray-200 h-12'>
         <div>Anywhere</div>
